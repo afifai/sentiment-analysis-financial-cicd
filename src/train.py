e83b90b4a9e65f7973c8b9719b5da128b821a466
yashintanovita <yashintanovitad@gmail.com>
2025-12-09 13:45:44 +0700

    Update model shinta

--- src/train.py
+++ src/train.py
@@ -123,7 +123,7 @@ def train_and_evaluate():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     # Model Pipeline
-    vectorizer = CountVectorizer(max_features=1000)
+    vectorizer = CountVectorizer(max_features=5000)
     X_train_vec = vectorizer.fit_transform(X_train)
     X_test_vec = vectorizer.transform(X_test)
     
