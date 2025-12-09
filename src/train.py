3112c5ab1543621c6c781b4769e20ecd25d07961
Mary Amalia Wauran <118521016+maryamalia@users.noreply.github.com>
2025-12-09 08:35:44 +0700

    change max feature

--- src/train.py
+++ src/train.py
@@ -81,7 +81,7 @@ def train_and_evaluate():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     # Model Pipeline
-    vectorizer = TfidfVectorizer(max_features=1000)
+    vectorizer = TfidfVectorizer(max_features=1500)
     X_train_vec = vectorizer.fit_transform(X_train)
     X_test_vec = vectorizer.transform(X_test)
     
