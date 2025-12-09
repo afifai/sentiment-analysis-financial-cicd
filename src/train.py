0d0f249d3c12bdc292d867ec611645511f15eb9f
SuhendraRasman <rasmansuhendra@gmail.com>
2025-12-09 13:54:18 +0700

    naive bayes7.py

--- src/train.py
+++ src/train.py
@@ -122,17 +122,20 @@ def train_and_evaluate():
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    # Model Pipeline
-vectorizer = TfidfVectorizer(
-    max_features=8000
-)
 
+def train_and_evaluate():
+    ...
+    # Model Pipeline
+    vectorizer = TfidfVectorizer(
+        max_features=8000
+    )
 
     X_train_vec = vectorizer.fit_transform(X_train)
     X_test_vec = vectorizer.transform(X_test)
-    
+
     model = MultinomialNB()
     model.fit(X_train_vec, y_train)
+
     
     # 4. Evaluasi
     y_pred = model.predict(X_test_vec)
